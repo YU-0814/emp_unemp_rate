57d867ad6939d1df26a5c7448936f6af95e9f871
kimtaekyeong12 <81560668+kimtaekyeong12@users.noreply.github.com>
2021-05-10 22:59:46 +0900

    'emp_unemp_rate'

--- emp_unemp_rate.py
+++ emp_unemp_rate.py
@@ -20,10 +20,20 @@ for content in table:
 for i in range(10):
     list_employment.append(j[5+5*i:9+5*i])
 
+list_unemployment = []
+table = soup.find_all('tr', {'data-id':'tr_149501_5'})
+for content in table:
+    j = content.get_text()
+
+for i in range(11):
+    list_unemployment.append(j[1+4*i:4+4*i])
+list_unemployment.pop(0)
+
 matplotlib.rcParams["axes.unicode_minus"]=False
 plt.rc('font', family ='Malgun Gothic')
 
 x = np.arange(10)
+y1 = list_unemployment
 y2 = list_employment
 
 fig, ax1 = plt.subplots()
@@ -35,6 +45,37 @@ ax1.set_xlabel('청년고용동향(고용률 및 실업률)')
 ax1.set_ylabel('고용률(%)')
 ax1.tick_params(axis='y', direction='in')
 
+ax2 = ax1.twinx()
+
+ax2.plot(x, y1, '-o', color='green', markersize=7, linewidth=5, alpha=0.7, label='실업률')
+ax2.set_ylim(0, 10)
+ax2.set_ylabel('실업률(%)')
+ax2.tick_params(axis='both', direction='in')
+
+ax2.set_zorder(ax2.get_zorder() + 10)
+ax2.patch.set_visible(False)
+
 ax1.legend(loc='upper left')
+ax2.legend(loc='upper right')
+
+for i, v in enumerate(x):
+    plt.text(v, y1[i], y1[i],
+             fontsize = 11,
+             color='black',
+             horizontalalignment='center',
+             verticalalignment='top')
 
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
